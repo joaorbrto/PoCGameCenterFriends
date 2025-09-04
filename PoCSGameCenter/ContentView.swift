9adb05d978788cb056f8e81c7b1047ec7a3fb35a
João Roberto Fernandes Magalhães <120526440+joaorbrto@users.noreply.github.com>
2025-09-04 08:35:25 -0300

    Initial Commit

---
+++ PoCSGameCenter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PoCSGameCenter
+//
+//  Created by Joao Roberto Fernandes Magalhaes on 04/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PoCSGameCenter/PoCSGameCenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoCSGameCenterApp.swift
+//  PoCSGameCenter
+//
+//  Created by Joao Roberto Fernandes Magalhaes on 04/09/25.
+//
+
+import SwiftUI
+
+@main
+struct PoCSGameCenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
